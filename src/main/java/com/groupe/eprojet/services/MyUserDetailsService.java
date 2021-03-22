43290ed36f5d21a5720f16b3c79f271941367fcb
Mourad <abdi.mourad02@gmail.com>
2021-03-22 12:43:13 +0100

    set up security login

--- src/main/java/com/groupe/eprojet/services/MyUserDetailsService.java
+++ src/main/java/com/groupe/eprojet/services/MyUserDetailsService.java
@@ -1,25 +1,26 @@
-//package com.groupe.eprojet.services;
-//
-//import com.groupe.eprojet.models.User;
-//import com.groupe.eprojet.models.UserPrincipal;
-//import com.groupe.eprojet.repositories.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//public class MyUserDetailsService implements UserDetailsService {
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-//        User user = userRepository.findUserByEmail(s);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("user not found");
-//        }
-//        return new UserPrincipal(user);
-//
-//    }
-//}
+package com.groupe.eprojet.services;
+
+import com.groupe.eprojet.models.User;
+import com.groupe.eprojet.models.UserPrincipal;
+import com.groupe.eprojet.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
